97954e19d891c5a6bf97808a2923cf2373794526
Elijah Smith <elijahsmith.m@gmail.com>
2022-04-22 09:55:37 -0500

    deletable logs

--- parkpal/src/components/TravelLog.js
+++ parkpal/src/components/TravelLog.js
@@ -1,9 +1,9 @@
 import React from "react";
 import TravelLogItem from "./TravelLogItem";
 
-function TravelLog({ logList }) {
+function TravelLog({ logList, handleClick }) {
   const renderLogs = logList.map((log) => {
-    return <TravelLogItem key={log.id} log={log} />;
+    return <TravelLogItem key={log.id} log={log} handleClick={handleClick} />;
   });
   return <div>Logs{renderLogs}</div>;
 }
